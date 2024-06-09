dec0c10d620f83a1d64c10ad209085a41c432464
Alexander R <a.reinecke@tu-braunschweig.de>
2024-06-09 14:56:59 +0200

    add training func

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import torch
 
+import keras
 from torch import nn
 
 from utility.file_utility import FileUtility
@@ -47,6 +48,42 @@ def encode_seq_list(sequence_list):
     return encoded_list
 
 
+def train_model(model, training_set, validation_set):
+    loss_fn = keras.losses.binary_crossentropy
+
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+    best_loss = 1_000_000
+
+    for epoch in range(100):
+        model.train(True)
+        # TRAIN ONE EPOCH
+        running_loss = 0.0
+        optimizer.zero_grad()
+
+        for seq, label in training_set:
+            output = model(seq)
+            loss = loss_fn(output, label)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+
+        # EVALUATE TRAINING
+        model.eval()
+        running_vloss = 0.0
+
+        with torch.no_grad():
+            for vseq, vlabel in validation_set:
+                output = model(vseq)
+                vloss = loss_fn(output, vlabel)
+                running_vloss += vloss.item()
+
+        if running_vloss < best_loss:
+            best_loss = running_vloss
+            model_path = 'model_{}_best.pt'.format(epoch)
+            torch.save(model.state_dict(), model_path)
+
+
 def main():
     # train data
     seq_train = FileUtility.load_list('dataset/train_seq.txt')
@@ -61,14 +98,23 @@ def main():
     # len_train = [len(s) for s in seq_train]
     # print("max len: {}".format(max(len_train)))
 
+    y_train = [float(number) for number in y_train]
+
+    y_test = [float(number) for number in y_test]
+
     encoded_train = encode_seq_list(seq_train)
+
+    training_set = zip(encoded_train, y_train)
+
     encoded_test = encode_seq_list(seq_test)
+
+    validation_set = zip(encoded_test, y_test)
 
     model = MLP()
 
     model.to(device)
 
-    model.train()
+    train_model(model, training_set, validation_set)
 
 
 if __name__ == '__main__':
